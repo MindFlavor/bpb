5e2014146dd206012d97b5ea29ccc8c240849334
Francesco Cogno <francesco.cogno@outlook.com>
2018-06-23 19:19:46 +0200

    Added support for constructor parameters

--- src/main.rs
+++ src/main.rs
@@ -19,9 +19,17 @@ pub struct Field {
 }
 
 #[derive(Debug, Clone, Deserialize)]
+pub struct ConstructorField {
+    pub name: String,
+    pub field_type: String,
+    pub trait_get: Option<String>,
+}
+
+#[derive(Debug, Clone, Deserialize)]
 pub struct Struct {
     pub name: String,
     pub extra_types: Vec<String>,
+    pub constructor_fields: Vec<ConstructorField>,
     pub fields: Vec<Field>,
 }
 
@@ -52,11 +60,16 @@ fn main() {
     // create the struct
     {
         output.push_str(&format!(
-            "pub struct {}{}\n{} {{",
+            "pub struct {}{}\n{} {{\n",
             stc.name,
             calculate_type_description(&stc, &[]),
             calculate_where(&stc)
         ));
+
+        // constructor types
+        for t in stc.constructor_fields.iter() {
+            output.push_str(&format!("\t{}: {},\n", t.name, t.field_type));
+        }
 
         // phantom types
         for f in stc.fields.iter().filter(|f| !f.optional) {
@@ -84,11 +97,16 @@ fn main() {
         ));
 
         output.push_str(&format!(
-            "\t pub(crate) fn new() -> {}{} {{\n\t\t{} {{\n",
+            "\t pub(crate) fn new({}) -> {}{} {{\n\t\t{} {{\n",
+            calculate_constructor_parameters(&stc),
             stc.name,
             calculate_type_description_all_no(&stc),
             stc.name
         ));
+
+        for cp in stc.constructor_fields.iter() {
+            output.push_str(&format!("\t\t\t{},\n", cp.name));
+        }
 
         for f in stc.fields.iter().filter(|f| !f.optional) {
             output.push_str(&format!("\t\t\tp_{}: PhantomData {{}},\n", f.name));
@@ -121,6 +139,20 @@ fn calculate_type(f: &Field) -> String {
         Some(_) => f.field_type.to_owned(),
         None => format!("Option<{}>", f.field_type),
     }
+}
+
+fn calculate_constructor_parameters(stc: &Struct) -> String {
+    let mut s = String::new();
+    let mut f_first = true;
+
+    for cp in stc.constructor_fields.iter() {
+        if !f_first {
+            s.push_str(", ");
+        }
+        s.push_str(&format!("{}: {}", cp.name, cp.field_type));
+        f_first = false;
+    }
+    s
 }
 
 fn calculate_type_description_all_no(stc: &Struct) -> String {
